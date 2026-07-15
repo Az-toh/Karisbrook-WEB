08c4f27fa880dbfa6246518c33ae67c8a875984e
admin <admin@DESKTOP-3SKE70S>
2026-07-15 15:19:04 +0300

    Добавил комменарий

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace KarisBrook
 {
     public class Program
     {
+        //Комментарий
         public static void Main(string[] args)
         {
             Console.WriteLine("=== Запуск приложения ===");
